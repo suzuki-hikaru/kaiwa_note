5235e85bb0e2fc7b9564643dae1dfed9e84077f0
hikaru <umisuki20@gmail.com>
2020-12-18 23:50:00 +0900

    feat:wordcloud追加のためルーティング追加

--- app/app.py
+++ app/app.py
@@ -55,3 +55,43 @@ def tw():
 
 
     return render_template("index.html",items=random_items)
+
+@app.route("/cwCreante", methods=['POST'])
+def cw():
+    # 名詞だけ抽出、単語をカウント
+    def counter(texts):
+        t = Tokenizer()
+        words_count = defaultdict(int)
+        words = []
+        for text in texts:
+            tokens = t.tokenize(text)
+            for token in tokens:
+                # 品詞から名詞だけ抽出
+                pos = token.part_of_speech.split(',')[0]
+                if pos in ['名詞']:
+                    # 必要ない単語を省く(実際の結果を見てから不必要そうな単語を記載しました)
+                    if token.base_form not in ["こと"]:
+                        words_count[token.base_form] += 1
+                        words.append(token.base_form)
+        return words_count, words
+
+    with open('./talk_data.txt', 'r') as f:
+        reader = csv.reader(f, delimiter='\t')
+        texts = []
+        for row in reader:
+            if(len(row) > 0):
+                text = row[0].split('http')
+                texts.append(text[0])
+
+    words_count, words = counter(texts)
+    text = ' '.join(words)
+
+    # fontは自分の端末にあるものを使用する
+    fpath = "./SourceHanSerifK-Light.otf"
+    wordcloud = WordCloud(background_color="white",
+                        font_path=fpath, width=900, height=500).generate(text)
+
+    wordcloud.to_file("./static/images/wordcloud.png")
+
+
+
